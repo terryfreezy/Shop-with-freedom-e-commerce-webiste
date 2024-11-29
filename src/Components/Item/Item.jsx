fbdfbc4962ff7d96a629587e698b9eb4f435a741
terryfreezy <freedomchinedu04@gmail.com>
2024-11-29 19:28:39 +0100

    first commit

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -9,8 +9,8 @@ const Item = (props) => {
       </Link>
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">${props.new_price}</div>
-        <div className="item-price-old">${props.old_price}</div>
+        <div className="item-price-new">N{props.new_price}</div>
+        <div className="item-price-old">N{props.old_price}</div>
       </div>
     </div>
   );
