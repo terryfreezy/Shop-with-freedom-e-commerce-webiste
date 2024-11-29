ec71e8ace18f2866352a1214457f5fca2cc11d63
terryfreezy <freedomchinedu04@gmail.com>
2024-11-29 19:05:48 +0100

    first commit

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+
+let data_product = [
+  {
+    id: 1,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p1_img,
+    new_price: 6500,
+    old_price: 8000,
+  },
+  {
+    id: 2,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p2_img,
+    new_price: 8500,
+    old_price: 12000,
+  },
+  {
+    id: 3,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p3_img,
+    new_price: 6000,
+    old_price: 10000,
+  },
+  {
+    id: 4,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p4_img,
+    new_price: 10000,
+    old_price: 15000,
+  },
+];
+
+export default data_product;
